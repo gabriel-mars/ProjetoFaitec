c6f8e7784a2d9e9558ffcfcfa5ff573a794bcaa7
gabrielsilva <gabrielrms@outlook.com.br>
2015-10-02 20:23:06 -0300

    Building fields preparo and rendimento

--- www/js/criarElementos.js
+++ www/js/criarElementos.js
@@ -15,9 +15,21 @@ function criarElementos(id){
     for(var i = 0; i < receita.length; i++){
        if(receita[i].id == id){
            var titulo = document.createTextNode(receita[i].nome);
+           var prep = document.createTextNode(receita[i].preparo);
+           var rendi = document.createTextNode(receita[i].rendimento);
+           
+           var p1 = document.createElement('p');
+           var p2 = document.createElement('p');
            var h1 = document.createElement('h1');
+           
            h1.appendChild(titulo);
+           p1.appendChild(prep);
+           p2.appendChild(rendi);
+           
            div.appendChild(h1);
+           div.appendChild(p1);
+           div.appendChild(p2);
+           
            return(receita[i]);
        }
     }  
