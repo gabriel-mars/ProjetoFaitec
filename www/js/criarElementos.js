e7ae3596a1c8b223e600d445887540c8a9bd9a4b
Gabriel Silva <gabrielrms@outlook.com.br>
2015-10-02 08:30:15 -0300

    Parsing the 'var id'

--- www/js/criarElementos.js
+++ www/js/criarElementos.js
@@ -3,10 +3,11 @@ window.onload = getHash();
 function getHash(){
     var hash = location.hash;
     var id = location.hash.split('#')[1];
+    id = parseInt(id, 10);
     
     criarElementos(id);
 }
 
 function criarElementos(id){
-    alert(id);   
+    console.log(id);   
 }
